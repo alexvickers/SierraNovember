8c150afc13fce2db505d599c0ba1dbb534e14769
alexvickers <alexvickers@gmail.com>
2019-10-27 23:02:07 -0300

    Removed (some) excess files and added SASS suport

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,17 @@
-import React from "react"
+import { Link } from 'gatsby';
+import React from 'react';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>NOPE</h1>
+    <p>
+      There's something wrong that isn't right. Please refer back to the{" "}
+      <Link to="/">homepage</Link>.
+    </p>
   </Layout>
 )
 
