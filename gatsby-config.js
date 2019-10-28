0012b66454c083cc0be27cd6a56ba11887da274f
alexvickers <alexvickers@gmail.com>
2019-10-28 11:59:29 -0300

    Replaced Favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,16 +20,10 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
-        short_name: `starter`,
+        short_name: `sierra`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+        icon: `src/images/mountain-solid.svg`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
