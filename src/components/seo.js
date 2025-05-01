4298e417571109f4af7499b2f53a82d6d561286f
Alexandre Aimbiré <alexvickers@gmail.com>
2025-05-01 16:59:34 -0300

    Fixed react-helmet for deploy

--- src/components/seo.js
+++ src/components/seo.js
@@ -7,7 +7,7 @@
 import { graphql, useStaticQuery } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
-import Helmet from 'react-helmet';
+import { Helmet } from 'react-helmet-async';
 
 function SEO({ description, lang, meta, title }) {
   const { site } = useStaticQuery(
